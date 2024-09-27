176145f2d3ca05e083a3790979ce79201d7415de
jaouad mhamdi <98467197+jm-hamdi@users.noreply.github.com>
2024-09-27 13:54:21 +0100

    ERP project

---
+++ src/main/java/com/example/SimpleERP/SimpleErpApplication.java
@@ -0,0 +1,13 @@
+package com.example.SimpleERP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleErpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SimpleERP/SimpleErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SimpleERP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
